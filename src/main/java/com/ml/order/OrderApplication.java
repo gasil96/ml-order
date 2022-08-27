3c67b5d6a7ff8aef2e32c7882841c122328e3e88
gabriel.silva <gabriel.silva@compasso.com.br>
2022-08-27 14:28:28 -0300

    add multiple datasources

--- src/main/java/com/ml/order/OrderApplication.java
+++ src/main/java/com/ml/order/OrderApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableCaching
+@EnableMongoRepositories
 @OpenAPIDefinition(info = @Info(title = "${application.title}", version = "${application.version}", description = "${application.description}"))
 public class OrderApplication {
 
